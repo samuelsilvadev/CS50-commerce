b11a5a3a49b83cef9c64d584170b902bb8df3155
Samuel Silva <samuelsilvawb@gmail.com>
2025-01-07 21:25:22 +0000

    feat: add active class on active URL

---
+++ auctions/templatetags/custom_tags.py
@@ -0,0 +1,16 @@
+from django import template
+from django.urls import reverse
+
+register = template.Library()
+
+
+@register.simple_tag(takes_context=True)
+def active_class(context, url_name):
+    """
+    Returns 'active' if the current request path matches the given URL name.
+    """
+    request = context['request']
+
+    if request.path == reverse(url_name):
+        return 'active'
+    return ''
