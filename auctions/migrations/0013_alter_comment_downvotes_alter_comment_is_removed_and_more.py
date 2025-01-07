9c895056b88b6055fc5e37562cf988b75da2d41b
Samuel Silva <samuelsilvawb@gmail.com>
2025-01-07 20:52:53 +0000

    style: format

--- auctions/migrations/0013_alter_comment_downvotes_alter_comment_is_removed_and_more.py
+++ auctions/migrations/0013_alter_comment_downvotes_alter_comment_is_removed_and_more.py
@@ -4,25 +4,24 @@ from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
-        ('auctions', '0012_alter_auction_end_date'),
+        ("auctions", "0012_alter_auction_end_date"),
     ]
 
     operations = [
         migrations.AlterField(
-            model_name='comment',
-            name='downvotes',
+            model_name="comment",
+            name="downvotes",
             field=models.IntegerField(default=0),
         ),
         migrations.AlterField(
-            model_name='comment',
-            name='is_removed',
+            model_name="comment",
+            name="is_removed",
             field=models.BooleanField(default=False),
         ),
         migrations.AlterField(
-            model_name='comment',
-            name='upvotes',
+            model_name="comment",
+            name="upvotes",
             field=models.IntegerField(default=0),
         ),
     ]
